621488dea4fb46705ced9283168629c7dfc2fad6
AniParemuzian <a.paremuzian@gmail.com>
2024-02-27 01:32:04 +0400

    full scss

--- src/HouseSlice/HouseSlice.jsx
+++ src/HouseSlice/HouseSlice.jsx
@@ -7,70 +7,70 @@ export default function HouseSlice() {
           id:1,  
           image:"./images/house1.jpg",
           name: "Meadow viwe",
-          discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-          price: "$570"
+          description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+          price: "$ 570.00"
         },
         {
             id:2,
             image:"./images/house2.jpg",
             name: "Orchard House",
-            discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-            price: "$570"
+            description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+            price: "$ 570.00"
         },
         {
             id:3,  
             image:"./images/house3.jpg",
             name: "School House",
-            discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-            price: "$570"
+            description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+            price: "$ 570.00"
         },
         {
            id:4,
            image:"./images/house4.jpg",
            name: "Meadow viwe",
-           discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-           price: "$570"
+           description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+           price: "$ 570.00"
         },
         {
           id:5,
           image:"./images/house5.jpg",
           name: "Orchard House",
-          discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-          price: "$570"
+          description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+          price: "$ 570.00"
         },
         {
          id:6,
          image:"./images/house6.jpg",
          name: "School House",
-         discription: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
-         price: "$570"
+         description: " 3 Bads |  3 Baths  |  2,532 Sq.Ft",
+         price: "$ 570.00"
         }
 
     ])
   return (
     <div className='HouseSlice'>
-      <div className="container">
-        <h2>Best Property Deals</h2>
-      {
-        houseSlice.map(house =>{
-          return (
-            <div className="HouseSlice__slice" key={house.id}>
-              <img src={house.image} />
-                            
-              <div className="HouseSlice__text">
+  <h2>Best Property Deals</h2>
+  <div className="container">
+    {
+      houseSlice.map(house => {
+        return (
+          <div className="HouseSlice__slice" key={house.id}>
+            <img src={house.image} alt={house.name} />
+            <div className="HouseSlice__text">
               <h3>{house.name}</h3>
-                 <p>{house.discription}</p>
-              </div>
-              
-              <div className="HouseSlice__price">
-                <p>{house.price}</p>
-              </div>
+              <p>{house.description}</p>
             </div>
-          )
-        })
-      }
-      <button>View All Property</button>
-      </div>
-    </div>
+            <div className="HouseSlice__price">
+              <p>{house.price}</p>
+            </div>
+          </div>
+        );
+      })
+    }
+  </div>
+  <div className="HouseSlice__button">
+  <button>View All Property</button>
+  </div>
+</div>
   )
 }
